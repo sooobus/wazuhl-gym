901da4159b2f7cee9b636f46c198dda3159aba9f
Valeriia Nemychnikova <sooobus@gmail.com>
2019-05-24 12:52:03 +0300

    Fix microbenchmarks

--- docker-compose.sh
+++ docker-compose.sh
@@ -6,5 +6,6 @@ rm mongo-c-driver-1.13.0.tar.gz
 git clone https://github.com/mongodb/mongo-cxx-driver.git --branch releases/stable --depth 1
 git clone https://github.com/BVLC/caffe.git
 git clone https://github.com/SavchenkoValeriy/wazuhl-llvm-test-suite.git suites/llvm-test-suite
-git clone http://hera:8080/gerrit/wazuhl-clang tools/clang
+cp CMakeLists_for_mb.txt suites/llvm-test-suite/MicroBenchmarks/CMakeLists.txt
+git clone https://github.com/SavchenkoValeriy/wazuhl-clang tools/clang
 docker-compose build
